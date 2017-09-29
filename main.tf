8cec75b5616aea3997151037a7df4b069193e060
danieljgraves <daniel.graves@rig.net>
2017-09-29 20:30:41 +0000

    updated provisioner script

--- main.tf
+++ main.tf
@@ -18,23 +18,23 @@ resource "aws_instance" "web" {
         PrincipalId = "AIDAJQPTTLIJNWDTZJBBQ"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-	"sudo apt-get -y update",
-	"sudo apt-get -y install apache2",
-#	"sudo add-apt-repository -y ppa:certbot/certbot",
+#  provisioner "remote-exec" {
+#    inline = [
 #	"sudo apt-get -y update",
-#	"sudo apt-get -y install python-certbot-apache",
-	"sudo wget https://dl.eff.org/certbot-auto",
-	"sudo apt-get -y update",
-	"sudo chmod a+x ./certbot-auto",
-#	"sudo ./certbot-auto certonly --apache --agree-tos --non-interactive --text --rsa-key-size 4096 --email daniel.graves@rig.net --webroot-path /var/www/html --domains \"site.dev.rig.net\""
-    ]
-    connection {
-        type = "ssh"
-        user = "ubuntu"
-        private_key = "${file("~/.ssh/deploy.pem")}"
-    }
-  }
+#	"sudo apt-get -y install apache2",
+##	"sudo add-apt-repository -y ppa:certbot/certbot",
+##	"sudo apt-get -y update",
+##	"sudo apt-get -y install python-certbot-apache",
+#	"sudo wget https://dl.eff.org/certbot-auto",
+#	"sudo apt-get -y update",
+#	"sudo chmod a+x ./certbot-auto",
+##	"sudo ./certbot-auto certonly --apache --agree-tos --non-interactive --text --rsa-key-size 4096 --email daniel.graves@rig.net --webroot-path /var/www/html --domains \"site.dev.rig.net\""
+#    ]
+#    connection {
+#        type = "ssh"
+#        user = "ubuntu"
+#        private_key = "${file("~/.ssh/deploy.pem")}"
+#    }
+#  }
 
 }
