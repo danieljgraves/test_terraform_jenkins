2670e0bfeb448947df85984f2046f54ae27f636a
danieljgraves <daniel.graves@rig.net>
2017-10-02 21:02:35 +0000

    Added ICMP to SG

--- terraform-plugin/security_groups.tf
+++ terraform-plugin/security_groups.tf
@@ -4,6 +4,15 @@ resource "aws_security_group" "web" {
   vpc_id      = "${var.vpc_id}"
 
   ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = [
+        "0.0.0.0/0",
+        ]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
